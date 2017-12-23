513f24e898f7f960a32e810c593780b5152c9286
Anonymous <user@example.com>
2017-12-23 23:08:10 +0900

    Replace default content with separated panels.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <h1>{{ msg }}</h1>
-      <h2>Essential Links</h2>
-      <ul>
-        <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-        <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-        <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-        <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-        <br>
-        <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-      </ul>
-      <h2>Ecosystem</h2>
-      <ul>
-        <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-        <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-        <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-      </ul>
+  <div id="app" style="width: 100%; height: 100%; display: flex; flex-flow: column nowrap;">
+    <div style="height: 32px;">
+      <div style="width: 100%; height: 100%; display: flex; flex-flow: row nowrap;">
+        <div style="width: 64px; background-color: red;">
+        </div>
+        <div style="flex: 1; background-color: blue;">
+        </div>
+      </div>
+    </div>
+    <div style="flex: 1;">
+      <div style="width: 100%; height: 100%; display: flex; flex-flow: row nowrap;">
+        <div style="width: 64px; background-color: green;">
+        </div>
+        <div style="flex: 1;">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -35,13 +31,19 @@ export default {
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 h1, h2 {
