0d962758fe41317596fd5a745c7f1e6365b5c955
Anonymous <user@example.com>
2017-12-24 16:17:16 +0900

    Add pitches pane.

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,56 @@
         </div>
       </div>
     </div>
-    <div style="flex: 1;">
+    <div style="flex: 1; height: 0;">
       <div style="width: 100%; height: 100%; display: flex; flex-flow: row nowrap;">
-        <div style="width: 64px; background-color: green;">
+        <div class="no-scrollbar" ref="pitchesPane" style="width: 64px; background-color: green;"
+             @scroll="onScroll_pitchesPane($event)">
+          <div
+             :style="{
+              'position': 'relative',
+              'width': '100%',
+              'height': (pitches.length * 20) + 'px',
+              }">
+            <template v-for="pitch in pitches">
+              <div :style="{
+                'position': 'absolute',
+                'left': '0px',
+                'bottom': ((pitch - pitchMin) * 20) + 'px',
+                'width': '100%',
+                'height': '20px',
+                'background-color': (
+                  [0, 2, 4, 5, 7, 9, 11].includes(((pitch % 12)+12)%12)
+                  ?
+                  'white'
+                  :
+                  'gray'
+                  ),
+                }">
+                {{pitch}}
+              </div>
+            </template>
+          </div>
         </div>
-        <div style="flex: 1;">
+        <div ref="notesPane"
+             style="flex: 1; overflow: scroll;"
+             @scroll="onScroll_notesPane($event)">
+          <div :style="{
+              'position': 'relative',
+              'width': '2000px',
+              'height': (pitches.length * 20) + 'px',
+              }">
+            <template v-for="note in notes">
+              <div :style="{
+                'position': 'absolute',
+                'left': '40px',
+                'bottom': ((note.pitch - pitchMin) * 20) + 'px',
+                'width': '20px',
+                'height': '20px',
+                'background-color': 'red',
+                }">   
+              </div>
+            </template>
+          </div>
         </div>
       </div>
     </div>
@@ -25,7 +70,43 @@ export default {
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
+      pitchMax: 9 * 12 - 1,
+      pitchMin: (-2) * 12,
+      notes: [
+        {
+          startAt: 1920,
+          duration: 1920,
+          pitch: 48,
+        },
+      ],
     };
+  },
+  computed: {
+    pitches() {
+      const pitches = [];
+      for (let i = this.pitchMin ; i <= this.pitchMax ; i++) {
+        pitches.push(i);
+      }
+      return pitches;
+    }
+  },
+  methods: {
+    onScroll_pitchesPane($event) {
+      this.sentFrom_pitchesPane = true;
+      if (this.sentFrom_notesPane) { // Prevent mutual dependency (which causes performance degradation)
+        this.sentFrom_notesPane = false;
+      } else {
+        this.$refs.notesPane.scrollTop = $event.target.scrollTop
+      }
+    },
+    onScroll_notesPane($event) {
+      this.sentFrom_notesPane = true;
+      if (this.sentFrom_pitchesPane) { // Prevent mutual dependency
+        this.sentFrom_pitchesPane = false;
+      } else {
+        this.$refs.pitchesPane.scrollTop = $event.target.scrollTop
+      }
+    },
   },
 };
 </script>
@@ -44,6 +125,14 @@ html, body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.no-scrollbar {
+  overflow-y: auto;
+}
+
+.no-scrollbar::-webkit-scrollbar {
+  display:none;
 }
 
 h1, h2 {
